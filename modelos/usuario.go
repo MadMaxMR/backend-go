840c055c2b30a386b73da28d610272797bf83dad
MadMax <64908632+MadMaxMR@users.noreply.github.com>
2022-03-18 23:02:33 -0500

    Update usuario.go

--- modelos/usuario.go
+++ modelos/usuario.go
@@ -21,3 +21,20 @@ type Usuarios struct {
 	Fecha_Registro   time.Time      `json:"fecha_registro" gorm:"type:timestamp;default:current_timestamp"`
 	Last_Login       time.Time      `json:"last_login" gorm:"type:timestamp"`
 }
+
+func Hash(password string) ([]byte, error) {
+	return bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+}
+
+func VerifyPassword(hashedPassword, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
+
+func BeforeSave(password string) string {
+	hasedPassword, err := Hash(password)
+	if err != nil {
+		return err.Error()
+	}
+	password = string(hasedPassword)
+	return password
+}
