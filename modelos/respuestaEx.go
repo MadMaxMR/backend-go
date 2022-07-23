2e6dba92b5e668476042b99978288718b0df2149
MadMaxMR <madmaxmolina14@gmail.com>
2022-07-23 17:22:29 -0500

    ""

--- modelos/respuestaEx.go
+++ modelos/respuestaEx.go
@@ -5,4 +5,6 @@ type RespuestaExs struct {
 	PreguntaExamensId uint   `json:"pregunta_ex_id" gorm:"type:int REFERENCES pregunta_examens(id) "`
 	Valor             bool   `json:"valor" gorm:"type:bool"`
 	Respuesta         string `json:"respuesta" gorm:"type:varchar(250)"`
+	Image			bool `json:"image" gorm:"type:bool"`
+	ImgLink			string `json:"img_link" gorm:"type:varchar(250)"`
 }
