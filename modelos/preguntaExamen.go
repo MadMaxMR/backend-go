0014f1b526c8f3e1c88f409fa8d7b53149e86576
crazyrmx1 <madmaxmolina14@gmail.com>
2022-08-24 23:13:09 -0500

    ""

--- modelos/preguntaExamen.go
+++ modelos/preguntaExamen.go
@@ -4,7 +4,6 @@ type PreguntaExamens struct {
 	ID           uint           `json:"id" gorm:"primary_key;auto_increment"`
 	ExamensId    uint           `json:"examen_id" gorm:"type:int REFERENCES examens(id) "`
 	NumQuestion  uint           `json:"num_question" gorm:"type:int"`
-	Pregunta     string         `json:"pregunta" gorm:"type:varchar(250);not null"`
 	Enunciado1   string         `json:"enunciado1" gorm:"type:varchar(250)"`
 	Grafico      string         `json:"grafico" gorm:"type:varchar(250)"`
 	Enunciado2   string         `json:"enunciado2" gorm:"type:varchar(250)"`
