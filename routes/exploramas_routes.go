763edf6d076d2313617007737d10a7be6335e745
crazyrmx1 <madmaxmolina14@gmail.com>
2023-10-01 22:27:36 -0500

    Indicadores

--- routes/exploramas_routes.go
+++ routes/exploramas_routes.go
@@ -1,1 +1,17 @@
 package routes
+
+import "net/http"
+
+func getIndicadores(w http.ResponseWriter, r *http.Request) {
+
+	return
+}
+
+/*
+select e.anio,e.id_uni,e.areas_id ,pe.cursos_id ,pe.temas_id,count(*) total from examens e
+		inner join examen_preguntas ep on ep.examens_id  = e.id
+		inner join pregunta_examens pe on pe.id = ep.pregunta_examens_id
+		where e.tipo_examen  = 'Admision' and e.cantidad_preguntas = e.limite_preguntas
+		group by e.anio, e.id_uni,e.areas_id,pe.cursos_id,pe.temas_id
+		order by  e.anio, pe.cursos_id,pe.temas_id
+*/
